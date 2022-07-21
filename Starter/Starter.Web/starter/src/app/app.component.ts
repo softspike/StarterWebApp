3a85a3681305f8d220086c61b1e75c716a22407b
efka <vilkinas@yahoo.com>
2022-07-21 12:24:05 +0100

    web-first approach - side nav layout

--- Starter/Starter.Web/starter/src/app/app.component.ts
+++ Starter/Starter.Web/starter/src/app/app.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  mobileQuery: MediaQueryList;
+  
+
+constructor(
+
+
+){
+
 }
+
+
+
+
+
+
+
+  ngOnInit() {
+
+  }
+
+
+
+
+}
+
