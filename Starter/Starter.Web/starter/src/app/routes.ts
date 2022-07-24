c4315da6c91e1594d5d55d882a2c8f2ad653e2ce
efka <vilkinas@yahoo.com>
2022-07-24 01:29:33 +0100

    setting up dashboard layout

--- Starter/Starter.Web/starter/src/app/routes.ts
+++ Starter/Starter.Web/starter/src/app/routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { AgencyTableComponent } from "./free-agency/agency-table/agency-table.component";
+import { HomeComponent } from './home/home.component';
 import { TournamentListComponent } from "./tournament-info/tournament-list/tournament-list.component";
 
 export const appRoutes: Routes = [
 
 
-   // {path: '**', component: TournamentListComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+
+    { path: 'home', component: HomeComponent, data: { title: 'StarterApp | Dashboard', breadcrumb: 'Dashboard > Join Us' },},
 
     { path: 'tournament/list', component: TournamentListComponent, data: { title: 'StarterApp | All Tournaments', breadcrumb: 'Dashboard > All Tournaments' },},
 
