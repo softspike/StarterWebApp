798d214ec8cc34462e8aa6d14e02b30737942513
efka <vilkinas@yahoo.com>
2022-07-26 11:37:52 +0100

    updates

--- Starter/Starter.Web/starter/src/app/free-agency/agency-button/agency-button.component.ts
+++ Starter/Starter.Web/starter/src/app/free-agency/agency-button/agency-button.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-agency-button',
   templateUrl: './agency-button.component.html',
   styleUrls: ['./agency-button.component.scss']
 })
-export class AgencyButtonComponent implements OnInit {
+export class AgencyButtonComponent implements OnInit, AfterViewInit {
 
   constructor() { }
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
   ngOnInit() {
+    
   }
 
 }
